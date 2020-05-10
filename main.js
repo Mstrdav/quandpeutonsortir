22e6d88998deddada091842c894fb4d3529f8680
Colin Geindre <colin.geindre@gmail.com>
2020-05-11 00:02:54 +0200

    demain

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 var timer = document.getElementById('timer');
 var ms = document.getElementById('ms');
 var m = document.getElementById('m');
-var dateSortie = new Date('2020-05-11T00:00:00');
+var dateSortie = new Date('2020-05-12T00:00:00');
 
 let interval = setInterval(function() {
     let now = new Date()
     let time = (dateSortie - now);
+    let heures = 23 - now.getHours();
     let minutes = 59 - now.getMinutes();
     let secondes = 59 - now.getSeconds();
     let milis = 999 - now.getMilliseconds();
-    ms.innerHTML = minutes + ':' + secondes + "'";
-    m.innerHTML = milis + "''";
+    ms.innerHTML = heures + ':' + minutes + "'" + secondes + "''";
+    m.innerHTML = milis;
 
     if(time < 0) {
         clearInterval(interval);
-        timer.innerHTML = '0';
+        timer.innerHTML = 'Libres.';
         document.body.classList = 'gradient';
+        timer.style.left = "50%";
     }
 }, 10);
