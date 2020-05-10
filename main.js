b9cabc08e9be0bee4b36934d9dfcff12c5b3894a
Colin Geindre <colin.geindre@gmail.com>
2020-05-10 23:46:09 +0200

    maj

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ var dateSortie = new Date('2020-05-11T00:00:00');
 let interval = setInterval(function() {
     let time = '' + (dateSortie - new Date());
     let minutes = time.substr(0,1);
-    let secondes = time.substr(3,2);
-    let milis = time.substr(5,2);
+    let secondes = time.substr(1,2);
+    let milis = time.substr(3,2);
     ms.innerHTML = minutes + ':' + secondes + "'";
     m.innerHTML = milis + "''";
 
